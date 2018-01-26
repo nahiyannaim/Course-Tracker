689160c50dfe5157dde9efdba5e9adbfd4d1057e
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-01-26 16:39:16 -0600

    update

--- app/src/main/java/com/example/uju/coursetracker/MainActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-
+//is master updated?
     }
 }
