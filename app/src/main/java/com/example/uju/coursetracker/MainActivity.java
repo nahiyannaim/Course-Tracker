f153a5723734a78f7fa372dc8d6e9d5393407aaf
nahiyannaim <35779850+nahiyannaim@users.noreply.github.com>
2018-01-26 16:10:59 -0600

    Nahiyan branch (#6)
    * kii
    
    loooool
    
    * l
    
    cos apparently making it isnt enough
    
    * Test
    
    * branch test
    
    * testing commit to iteration1

--- app/src/main/java/com/example/uju/coursetracker/MainActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/MainActivity.java
@@ -9,10 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        //uju's comment
-        //christian's comment
-        //Nahiyan's comment
-        //FRANCIS IS DA SHIII
 
     }
 }
