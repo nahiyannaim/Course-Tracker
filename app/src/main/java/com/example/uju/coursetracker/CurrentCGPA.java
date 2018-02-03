f45194d05c8b5b98d405b3d82366cde0660b6ceb
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-02-02 18:23:38 -0600

    Created CurrentCGPA activity and its basic UI

---
+++ app/src/main/java/com/example/uju/coursetracker/CurrentCGPA.java
@@ -0,0 +1,13 @@
+package com.example.uju.coursetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentCGPA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_cgpa);
+    }
+}
