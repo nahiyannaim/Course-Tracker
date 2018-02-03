0ceba1046215a34df894e4f65c2aca024653e7bc
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-02-02 19:27:57 -0600

    Created a temporary navigation bettween CurrentCGPA activity and main activity to test currentCGPA page of the app.

--- app/src/main/java/com/example/uju/coursetracker/MainActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.uju.coursetracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+
+        //This section is to only create a navigation between home page and currCGPA page Delete it after getting the real home page
+        Button but = (Button) findViewById(R.id.goToCurrButton);
+
+        but.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, CurrentCGPA.class));
+            }
+        });
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     }
 }
