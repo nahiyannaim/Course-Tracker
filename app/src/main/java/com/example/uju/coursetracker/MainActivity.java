9e8685ad09c161163e68cf3b10ef83a0a879ca47
FOkoro <okorof@github.com>
2018-02-04 01:12:57 -0600

    Changes to The Main_activity
    Changed activity layout , created tabs and navigation drawer

--- app/src/main/java/com/example/uju/coursetracker/MainActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.uju.coursetracker;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout dLayout;
+    private ActionBarDrawerToggle dToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        dLayout = (DrawerLayout)findViewById(R.id.drawer);
+        dToggle = new ActionBarDrawerToggle(MainActivity.this,dLayout,R.string.open,R.string.close);
+        dLayout.addDrawerListener(dToggle);
+        dToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(dToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
