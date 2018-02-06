9cc01f4834d7da6ca1ef213532ae5a199da51ef9
Uju <hillary.nnamani@hotmail.com>
2018-02-06 14:19:32 -0600

    just saving it

--- app/src/main/java/com/example/uju/coursetracker/CurrentCGPA.java
+++ app/src/main/java/com/example/uju/coursetracker/CurrentCGPA.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CurrentCGPA extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,4 +46,6 @@ public class CurrentCGPA extends AppCompatActivity {
 
 
     }
+
+
 }
