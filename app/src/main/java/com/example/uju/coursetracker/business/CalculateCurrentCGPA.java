6454e04706eb882a6f625218164781aabb01c888
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-02-09 12:59:30 -0600

    Added business current cgpa calculate class.

--- app/src/main/java/com/example/uju/coursetracker/business/CalculateCurrentCGPA.java
+++ app/src/main/java/com/example/uju/coursetracker/business/CalculateCurrentCGPA.java
@@ -3,5 +3,6 @@ package com.example.uju.coursetracker.business;
 
 public class CalculateCurrentCGPA
 {
-    //this is where we will calculate the current CGPA based on the already completed courses of the user
+    //This is where we will calculate the current CGPA based on the already completed courses of the user
+
 }
