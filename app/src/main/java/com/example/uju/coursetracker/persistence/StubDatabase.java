439e7ad2c806d178860cfdecb9ed4c1f2dc4ffeb
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-02-09 13:00:31 -0600

    Added the stub database.

--- app/src/main/java/com/example/uju/coursetracker/persistence/StubDatabase.java
+++ app/src/main/java/com/example/uju/coursetracker/persistence/StubDatabase.java
@@ -4,6 +4,6 @@ package com.example.uju.coursetracker.persistence;
 
 public class StubDatabase
 {
-    // This is where our stub will be located.
-    // This stub database will have a set of initial contents and will provide the data for other classes
+    // This is the stub database for the project
+    // This stub database will have a set of initial contents and will provide the data for other classes.
 }
