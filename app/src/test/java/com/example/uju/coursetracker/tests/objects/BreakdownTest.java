324b3f393c84796a292b62a40ddda561b1788e27
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-02-10 13:26:54 -0600

    Completed all Unit tests for Course class and Breakdown class

--- app/src/test/java/com/example/uju/coursetracker/tests/objects/BreakdownTest.java
+++ app/src/test/java/com/example/uju/coursetracker/tests/objects/BreakdownTest.java
@@ -1,9 +1,26 @@
 package com.example.uju.coursetracker.tests.objects;
 
 
+import com.example.uju.coursetracker.objects.Breakdown;
+import com.example.uju.coursetracker.objects.Course;
+
 import junit.framework.TestCase;
 
 public class BreakdownTest extends TestCase
 {
+    public void testBreakdown()
+    {
+        System.out.println("Starting Object Test: testBreakdown");
 
+        Breakdown breakdown = new Breakdown("Math 1500", "Term Test 2", 0.25);
+        Breakdown breakdown2 =  breakdown;
+
+        assertNotNull(breakdown);
+        assertEquals("Math 1500", breakdown.getCourseID());
+        assertEquals("Term Test 2", breakdown.getBreakdownName());
+        assertEquals(0.25, breakdown.getBreakdownPercentage());
+        assertSame(breakdown, breakdown2);
+
+        System.out.println("Finished Test");
+    }
 }
