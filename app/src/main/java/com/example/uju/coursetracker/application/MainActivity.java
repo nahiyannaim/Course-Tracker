bb60bd4fb53739d6dc6dff302fd4dc252b9329f0
Uju <hillary.nnamani@hotmail.com>
2018-02-10 20:18:04 -0600

    deleted the results xml and java class did the UI and the functionalities of the currentCGPA page

--- app/src/main/java/com/example/uju/coursetracker/application/MainActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/application/MainActivity.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 
 import com.example.uju.coursetracker.presentation.CurrentCGPAActivity;
 import com.example.uju.coursetracker.R;
-import com.example.uju.coursetracker.presentation.ResultspageActivity;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -51,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
 
             case R.id.cgpaPred:
-                Intent res = new Intent(MainActivity.this, ResultspageActivity.class);
+                Intent res = new Intent(MainActivity.this, CurrentCGPAActivity.class);
                 startActivity(res);
                 break;
 
