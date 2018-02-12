ab221855fbd57f0bad2acb96e0282bad691094aa
Hintaycl <35672198+Hintaycl@users.noreply.github.com>
2018-02-11 18:54:40 -0600

    Pushing to my own branchfrom my main computer

--- app/src/main/java/com/example/uju/coursetracker/business/AccessCourses.java
+++ app/src/main/java/com/example/uju/coursetracker/business/AccessCourses.java
@@ -1,175 +1,101 @@
 package com.example.uju.coursetracker.business;
 
-import java.util.ArrayList;
 import java.util.List;
-import com.example.uju.coursetracker.presentation.MainActivity;
+
+import com.example.uju.coursetracker.application.MainActivity;
 import com.example.uju.coursetracker.application.DatabaseService;
 import com.example.uju.coursetracker.objects.Course;
-import com.example.uju.coursetracker.persistence.DataAccess;
-
+import com.example.uju.coursetracker.persistence.StubDatabase;
 
 public class AccessCourses
 {
-    private DataAccess dataAccess;
-    private List<Course> completedCourses;
-    private List<Course> currentCourses;
+    private StubDatabase dataAccess;
+    private List<Course> courses;
     private Course course;
-    private Course course2;
-    private int count;
-    private int count2;
+    private int currentCourse;
 
     public AccessCourses()
     {
-        dataAccess = DatabaseService.getDataAccess(MainActivity.dbName);
-        completedCourses = null;
-        currentCourses = null;
+        dataAccess = (StubDatabase) DatabaseService.getDataAccess(MainActivity.dbName);
+        courses = null;
         course = null;
-        count = 0;
-        count2 = 0;
+        currentCourse = 0;
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    //   FOR COMPLETED COURSES
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    public String getCompletedCoursesSeq(List<Course> courses)
+    public String getCourses(List<Course> courses)
     {
-        if(courses != null)
-        {
-            courses.clear();
-        }
-
-        return dataAccess.getCompletedCoursesSeq(courses);
+        courses.clear();
+        return dataAccess.getCourseSequential(courses);
     }
 
-    public String insertCompletedCourse(Course currentCourse)
-    {
-        return dataAccess.insertCompletedCourse(currentCourse);
-    }
-
-    public String updateCompletedCourse(Course currentCourse)
-    {
-        return dataAccess.updateCompletedCourse(currentCourse);
-    }
-
-    public String deleteCompletedCourse(Course currentCourse)
-    {
-        return dataAccess.deleteCompletedCourse(currentCourse);
-    }
-
-    public Course getSequentialCompleted()
+    public Course getSequential()
     {
         String result = null;
-        if (completedCourses == null)
+        if (courses == null)
         {
-            // the following line was added as a result of a failing test in AccessCoursesTest!
-            completedCourses = new ArrayList<Course>();
-            result = dataAccess.getCompletedCoursesSeq(completedCourses);
-            count = 0;
+            result = dataAccess.getCourseSequential(courses);
+            currentCourse = 0;
         }
-        if (count < completedCourses.size())
+        if (currentCourse < courses.size())
         {
-            course = completedCourses.get(count);
-            count++;
+            course = (Course) courses.get(currentCourse);
+            currentCourse++;
         }
         else
         {
-            completedCourses = null;
+            courses = null;
             course = null;
-            count = 0;
+            currentCourse = 0;
         }
         return course;
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    //   FOR CURRENT COURSES
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    public String getCurrentCoursesSeq(List<Course> courses)
+    public Course getRandom(String courseID)
     {
-        if(courses != null)
+        courses = dataAccess.getCourseRandom(new Course(courseID));
+        currentCourse = 0;
+        if (currentCourse < courses.size())
         {
-            courses.clear();
-        }
-        return dataAccess.getCurrentCoursesSeq(courses);
-    }
-
-    public String insertCurrentCourse(Course currentCourse)
-    {
-        return dataAccess.insertCurrentCourse(currentCourse);
-    }
-
-    public String updateCurrentCourse(Course currentCourse)
-    {
-        return dataAccess.updateCurrentCourse(currentCourse);
-    }
-
-    public String deleteCurrentCourse(Course currentCourse)
-    {
-        return dataAccess.deleteCurrentCourse(currentCourse);
-    }
-
-    public Course getSequentialCurrent()
-    {
-        String result = null;
-        if (currentCourses == null)
-        {
-            // the following line was added as a result of a failing test in AccessCoursesTest!
-            currentCourses = new ArrayList<Course>();
-            result = dataAccess.getCurrentCoursesSeq(currentCourses);
-            count2 = 0;
-        }
-        if (count2 < currentCourses.size())
-        {
-            course2 = currentCourses.get(count2);
-            count2++;
+            course = (Course) courses.get(currentCourse);
+            currentCourse++;
         }
         else
         {
-            currentCourses = null;
-            course2 = null;
-            count2 = 0;
+            courses = null;
+            course = null;
+            currentCourse = 0;
         }
-        return course2;
+        return course;
     }
 
-    public String validateCourseData(Course course)
+    public String insertCompletedCourse(Course currentCourse)
     {
-        CalculateCurrentCGPA temp = new CalculateCurrentCGPA();
-        boolean flag =  false;
-        String result = null;
+        return dataAccess.insertOldCourse(currentCourse);
+    }
 
-        if (course.getCourseID().length() == 0)
-        {
-            result = "Please enter a valid Course ID.";
-        }
-        else
-        {
-            if (course.getGrade().length() == 0)
-            {
-                result = "Please enter a valid grade.";
-            }
+    public String insertNewCourse(Course currentCourse)
+    {
+        return dataAccess.insertNewCourse(currentCourse);
+    }
 
-            for (int j = 0; j < temp.grades.length; j++)
-            {
-                if (!(course.getGrade().equalsIgnoreCase(temp.grades[j])))
-                {
-                    flag = true;
-                }
-                else
-                {
-                    flag = false;
-                    break;
-                }
-            }
+    public String updateCompletedCourse(Course currentCourse)
+    {
+        return dataAccess.updateOldCourse(currentCourse);
+    }
 
-            if (flag)
-            {
-                result = "Please enter a valid grade.";
-            }
-        }
+    public String updateNewCourse(Course currentCourse)
+    {
+        return dataAccess.updateNewCourse(currentCourse);
+    }
 
-        return result ;
+    public String deleteCompletedCourse(Course currentCourse)
+    {
+        return dataAccess.deleteOldCourse(currentCourse);
+    }
+
+    public String deleteNewCourse(Course currentCourse)
+    {
+        return dataAccess.deleteNewCourse(currentCourse);
     }
 
 }
