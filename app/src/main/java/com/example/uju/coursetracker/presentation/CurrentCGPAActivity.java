41506032fd8eadbcad764640100271da3e84f284
hintaycl <hintaycl@myumanitoba.ca>
2018-02-12 19:39:56 -0600

    Update working and currGPA syncs with My Courses

--- app/src/main/java/com/example/uju/coursetracker/presentation/CurrentCGPAActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/presentation/CurrentCGPAActivity.java
@@ -2,6 +2,7 @@ package com.example.uju.coursetracker.presentation;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 
 import com.example.uju.coursetracker.R;
+import com.example.uju.coursetracker.application.DatabaseService;
 import com.example.uju.coursetracker.application.MainActivity;
 import com.example.uju.coursetracker.objects.Course;
 import com.example.uju.coursetracker.persistence.StubDatabase;
@@ -30,10 +32,10 @@ public class CurrentCGPAActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_cgpa);
 
-        StubDatabase stubdb = new StubDatabase("oldcrs");
+        //        StubDatabase stubdb = new StubDatabase("oldcrs");
         TextView tv = (TextView) findViewById(R.id.textView4);
-        stubdb.open("oldcrs");
-        double currCGPA = calculate(stubdb.getOldCourses());
+//        stubdb.open("oldcrs");
+        double currCGPA = calculate(DatabaseService.getDataAccess("MyCourses").getOldCourses());
         tv.setText(Double.toString(currCGPA));
 
         Button button1 = findViewById(R.id.button);
@@ -45,34 +47,9 @@ public class CurrentCGPAActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();  // Always call the superclass method first
-        setContentView(R.layout.activity_current_cgpa);
-
-        StubDatabase stubdb = new StubDatabase("oldcrs");
-        TextView tv = (TextView) findViewById(R.id.textView4);
-        stubdb.open("oldcrs");
-        double currCGPA = calculate(stubdb.getOldCourses());
-        tv.setText(Double.toString(currCGPA));
-
-        Button button1 = findViewById(R.id.button);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToPredictedCGPA();
-            }
-        });
-    }
-
-
     private void goToPredictedCGPA() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-
-
-
 
 }
