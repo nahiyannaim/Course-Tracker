6bdb8d2c97287716d6cc7994259e7dd4b3d076fd
hintaycl <hintaycl@myumanitoba.ca>
2018-03-02 10:41:35 -0600

    Finished setting up the database. Not done with implementing and testing it

--- app/src/main/java/com/example/uju/coursetracker/application/DatabaseService.java
+++ app/src/main/java/com/example/uju/coursetracker/application/DatabaseService.java
@@ -7,33 +7,53 @@ import com.example.uju.coursetracker.persistence.DataAccessObject;
 public class DatabaseService
 {
     //TOD0 CHANGE VARIABLE TYPE FROM StubDatabase to DataAccess
-    private static StubDatabase dataAccessService = null;
+    //private static StubDatabase dataAccessService = null;
+    private static DataAccess dataAccessService = null;
 
-    public static StubDatabase createDataAccess(String dbName)
+//    public static StubDatabase createDataAccess(String dbName)
+//    {
+//        if (dataAccessService == null)
+//        {
+//            dataAccessService = new StubDatabase(dbName);
+//            dataAccessService.open(dbName);
+//        }
+//        return dataAccessService;
+//    }
+
+//    public static StubDatabase getDataAccess(String dbName)
+//    {
+//        if (dataAccessService == null)
+//        {
+//            System.out.println("Connection to data access has not been established.");
+//            System.exit(1);
+//        }
+//        return dataAccessService;
+//    }
+
+    //For Iteration 2
+    //***************************************************************************************
+    public static DataAccess createDataAccess(String dbName)
     {
         if (dataAccessService == null)
         {
-            dataAccessService = new StubDatabase(dbName);
-            dataAccessService.open(dbName);
+            dataAccessService = new DataAccessObject(dbName);
+            dataAccessService.open(MainActivity.getDBPathName());
         }
         return dataAccessService;
     }
 
-    //For Iteration 2
-    //***************************************************************************************
-//    public static DataAccess createDataAccess(DataAccess alternateDataAccessService)
-//    {
-//        if (dataAccessService == null)
-//        {
-//            dataAccessService = alternateDataAccessService;
-//            dataAccessService.open(Main.getDBPathName());
-//        }
-//        return dataAccessService;
-//    }
+    public static DataAccess createDataAccess(DataAccess alternateDataAccessService)
+    {
+        if (dataAccessService == null)
+        {
+            dataAccessService = alternateDataAccessService;
+            dataAccessService.open(MainActivity.getDBPathName());
+        }
+        return dataAccessService;
+    }
     //************************************************************************************
 
-
-    public static StubDatabase getDataAccess(String dbName)
+    public static DataAccess getDataAccess(String dbName)
     {
         if (dataAccessService == null)
         {
@@ -51,5 +71,6 @@ public class DatabaseService
         }
         dataAccessService = null;
     }
+
 }
 
