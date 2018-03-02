37b7007baf5e8e856d294844f6aec7cdc0952042
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-03-02 17:21:18 -0600

    Saving changes for Current Courses SQL

--- app/src/main/java/com/example/uju/coursetracker/presentation/PredictNextCGPAResultsActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/presentation/PredictNextCGPAResultsActivity.java
@@ -5,39 +5,40 @@ import android.os.Bundle;
 import android.widget.TextView;
 import com.example.uju.coursetracker.R;
 import com.example.uju.coursetracker.application.DatabaseService;
+import com.example.uju.coursetracker.application.MainActivity;
 import com.example.uju.coursetracker.business.CalculateCurrentCGPA;
 import com.example.uju.coursetracker.business.PredictNextCGPA;
 
 
 public class PredictNextCGPAResultsActivity extends AppCompatActivity
 {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState)
-//    {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_predict_next_cgpa_results);
-//
-//        TextView tv = (TextView) findViewById(R.id.predictResult);
-//
-//        double currCGPA = CalculateCurrentCGPA.calculate(DatabaseService.getDataAccess("MyCourses").getOldCourses());
-//        int totalCoursesCompleted = (DatabaseService.getDataAccess("MyCourses").getOldCourses()).size();
-//        double predictedCGPA = PredictNextCGPA.calculate(DatabaseService.getDataAccess("MyCourses").getNewCourses(), currCGPA, totalCoursesCompleted);
-//        int currentCourseListSize = (DatabaseService.getDataAccess("MyCourses").getNewCourses()).size();
-//
-//        if(currentCourseListSize <= 0)
-//        {
-//            MessagesActivity.warning(this, "No Current Courses");
-//        }
-//
-//        if(predictedCGPA != -1.0)
-//        {
-//            tv.setText(Double.toString(predictedCGPA));
-//        }
-//        else
-//        {
-//            tv.setText("0.0");
-//        }
-//
-//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_predict_next_cgpa_results);
+
+        TextView tv = (TextView) findViewById(R.id.predictResult);
+
+        double currCGPA = CalculateCurrentCGPA.calculate(DatabaseService.getDataAccess(MainActivity.getDBPathName()).getCompletedCourses());
+        int totalCoursesCompleted = (DatabaseService.getDataAccess(MainActivity.getDBPathName()).getCompletedCourses()).size();
+        double predictedCGPA = PredictNextCGPA.calculate(DatabaseService.getDataAccess(MainActivity.getDBPathName()).getCurrentCourses(), currCGPA, totalCoursesCompleted);
+        int currentCourseListSize = (DatabaseService.getDataAccess(MainActivity.getDBPathName()).getCurrentCourses()).size();
+
+        if(currentCourseListSize <= 0)
+        {
+            MessagesActivity.warning(this, "No Current Courses");
+        }
+
+        if(predictedCGPA != -1.0)
+        {
+            tv.setText(Double.toString(predictedCGPA));
+        }
+        else
+        {
+            tv.setText("0.0");
+        }
+
+    }
 }
