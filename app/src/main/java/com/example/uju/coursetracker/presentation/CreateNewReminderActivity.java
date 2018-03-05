ef99bf1daf081b8aff19fdfe2a05cd598d3d997b
Uju <hillary.nnamani@hotmail.com>
2018-03-05 16:04:06 -0600

    updated with daniels's code

--- app/src/main/java/com/example/uju/coursetracker/presentation/CreateNewReminderActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/presentation/CreateNewReminderActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,15 +18,21 @@ import android.widget.TextView;
 import com.example.uju.coursetracker.R;
 import com.example.uju.coursetracker.business.AccessCourses;
 import com.example.uju.coursetracker.objects.Course;
+import com.example.uju.coursetracker.objects.Reminder;
 
 import java.util.ArrayList;
+import android.widget.AdapterView.OnItemSelectedListener;
+
 
 public class CreateNewReminderActivity extends AppCompatActivity {
 
     private AccessCourses accessNewCourses;
     private ArrayList<Course> newCourseList;
     private ArrayAdapter<Course> courseArrayAdapter;
+    private ArrayAdapter<CharSequence> RemTypeAdapter;
     private String newCoursesDBName = "new";
+    private Course selectedCourse;
+    private EditText selectedRemType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +44,7 @@ public class CreateNewReminderActivity extends AppCompatActivity {
         //current semester courseList
         newCourseList = new ArrayList<Course>();
 
-        String result2 = accessNewCourses.getCourses(newCourseList, newCoursesDBName);
+        String result2 = accessNewCourses.getCurrentCoursesSeq(newCourseList);
         if (result2 != null) {
             MessagesActivity.fatalError(this, result2);
         } else {
@@ -58,13 +66,10 @@ public class CreateNewReminderActivity extends AppCompatActivity {
             final ListView listView2 = findViewById(R.id.printComplCourses);
             listView2.setAdapter(courseArrayAdapter);
 
+            createSpinner();
+
         }
 
-        Spinner remTypeSpinner = findViewById(R.id.RemSpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.reminderTypes, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        remTypeSpinner.setAdapter(adapter);
 
         Button doneButton = findViewById(R.id.RemDoneButton);
         doneButton.setOnClickListener(new View.OnClickListener()
@@ -76,8 +81,38 @@ public class CreateNewReminderActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void createSpinner()
+    {
+        Spinner remTypeSpinner = findViewById(R.id.RemSpinner);
+        final ArrayAdapter<CharSequence> RemTypeAdapter = ArrayAdapter.createFromResource(this,
+                R.array.reminderTypes, android.R.layout.simple_spinner_item);
+        RemTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        remTypeSpinner.setAdapter(RemTypeAdapter);
+        remTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+
+                CharSequence rem = RemTypeAdapter.getItem(pos);
+                selectedRemType = findViewById(R.id.editText2);
+                selectedRemType.setText(rem.toString());
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+
+    }
+
     private void goToDueDatesPage() {
         Intent intent = new Intent(this, DueDatesActivity.class);
         startActivity(intent);
     }
+
+
 }
