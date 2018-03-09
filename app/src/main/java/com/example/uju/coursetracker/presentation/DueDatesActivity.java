b0d6e478de0f416964ec46697b8abb6179ee863d
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-03-09 14:58:12 -0600

    cosmetics

--- app/src/main/java/com/example/uju/coursetracker/presentation/DueDatesActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/presentation/DueDatesActivity.java
@@ -68,6 +68,8 @@ public class DueDatesActivity extends AppCompatActivity {
                     text1.setText(reminderList.get(position).getCourseID());
                     text2.setText(reminderList.get(position).getReminderType() +" Due: "+reminderList.get(position).getDueDate());
 
+//                    Log.d("myTag", reminderList.get(position).getDueDate());
+//                    Log.d("myTag", reminderList.size()+"");
 
                     return view;
 
