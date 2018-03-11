9927c94a379e2eb54fb67f1e06b0355cea525784
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-03-11 17:28:14 -0500

    AccessRemindersTest completed

--- app/src/test/java/com/example/uju/coursetracker/tests/business/AccessRemindersTest.java
+++ app/src/test/java/com/example/uju/coursetracker/tests/business/AccessRemindersTest.java
@@ -24,21 +24,64 @@ public class AccessRemindersTest extends TestCase
 
         DatabaseService.closeDataAccess();
 
-        System.out.println("\nStarting test AccessReminders");
+        System.out.println("\nStarting test AccessReminders for: test1");
 
         DatabaseService.createDataAccess(new StubDatabase(dbName));
 
         ar = new AccessReminders();
 
+
+        // Accessing the 1st reminder
         reminder = ar.getSequentialReminders();
 
+        assertNotNull(ar);
+        assertNotNull(reminder);
         assertEquals("SOCO 1200", reminder.getCourseID());
         assertEquals("Assignment", reminder.getReminderType());
         assertEquals("01/02/2018", reminder.getDueDate());
 
+
+
+        // Accessing the 3rd reminder
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+
+        assertNotNull(ar);
+        assertNotNull(reminder);
+        assertEquals("SOCO 1200", reminder.getCourseID());
+        assertEquals("Midterm", reminder.getReminderType());
+        assertEquals("02/20/2018", reminder.getDueDate());
+
+
+
+        // Accessing the last reminder
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+        reminder = ar.getSequentialReminders();
+
+        assertNotNull(ar);
+        assertNotNull(reminder);
+        assertEquals("ENGL 1300", reminder.getCourseID());
+        assertEquals("Midterm", reminder.getReminderType());
+        assertEquals("02/19/2018", reminder.getDueDate());
+
+
+
+        // Finished traversing reminders list in database after 13 items
+        reminder = ar.getSequentialReminders();
+
+        assertNull(reminder);
+        assertNotNull(ar);
+
+
         // ... ADD MORE
-
-
 
         DatabaseService.closeDataAccess();
         System.out.println("Finished test AccessReminders");
