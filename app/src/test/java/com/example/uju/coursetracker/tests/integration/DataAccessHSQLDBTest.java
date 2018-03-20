7b19640dd5a108ce2d5b381b8d0d002dd2fc3cd2
Nahiyan Naim <nahiyannaim@yahoo.com>
2018-03-19 22:41:38 -0500

    Added some code for Integration testing

--- app/src/test/java/com/example/uju/coursetracker/tests/integration/DataAccessHSQLDBTest.java
+++ app/src/test/java/com/example/uju/coursetracker/tests/integration/DataAccessHSQLDBTest.java
@@ -1,33 +1,37 @@
 package com.example.uju.coursetracker.tests.integration;
 
+import com.example.uju.coursetracker.application.DatabaseService;
+import com.example.uju.coursetracker.persistence.DataAccess;
+import com.example.uju.coursetracker.presentation.MainActivity;
+import com.example.uju.coursetracker.tests.persistence.StubDatabaseTest;
 import junit.framework.TestCase;
 
 
 public class DataAccessHSQLDBTest extends TestCase
 {
-//    private static String dbName = Main.dbName;
-//
-//    public DataAccessHSQLDBTest(String arg0)
-//    {
-//        super(arg0);
-//    }
-//
-//    public void testDataAccess()
-//    {
-//        DataAccess dataAccess;
-//
-//        Services.closeDataAccess();
-//
-//        System.out.println("\nStarting Integration test DataAccess (using default DB)");
-//
-//        // Use the following two statements to run with the real database
-//        Services.createDataAccess(dbName);
-//        dataAccess = Services.getDataAccess(dbName);
-//
-//        DataAccessTest.dataAccessTest(dataAccess);
-//
-//        Services.closeDataAccess();
-//
-//        System.out.println("Finished Integration test DataAccess (using default DB)");
-//    }
+    private static String dbName = MainActivity.dbName;
+
+    public DataAccessHSQLDBTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    public void testDataAccess()
+    {
+        DataAccess dataAccess;
+
+        DatabaseService.closeDataAccess();
+
+        System.out.println("\nStarting Integration test DataAccess (using default DB)");
+
+        // Use the following two statements to run with the real database
+        DatabaseService.createDataAccess(dbName);
+        dataAccess = DatabaseService.getDataAccess(dbName);
+
+        StubDatabaseTest.dataAccessTest(dataAccess);
+
+        DatabaseService.closeDataAccess();
+
+        System.out.println("Finished Integration test DataAccess (using default DB)");
+    }
 }
