289c0eec12f1a22364f628417e6d76152d351682
daniel <dnlrchrds92@gmail.com>
2018-03-21 09:29:35 -0500

    Merge branch 'Nahiyan' of https://github.com/uju96/courseTracker into Daniel
    # Conflicts:
    #	app/src/main/java/com/example/uju/coursetracker/presentation/DueDatesActivity.java
    #	app/src/main/res/layout/activity_due_dates.xml
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/uju/coursetracker/presentation/PickMapActivity.java
@@ -0,0 +1,15 @@
+package com.example.uju.coursetracker.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.uju.coursetracker.R;
+
+public class PickMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_map);
+    }
+}
