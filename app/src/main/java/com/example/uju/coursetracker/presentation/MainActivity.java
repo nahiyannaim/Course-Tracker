9b29d9c3efa01bc951303fa88002604f78400e1d
FOkoro <okorof@github.com>
2018-03-22 05:24:44 -0500

    Merge branch 'Daniel' of https://github.com/uju96/courseTracker into Francis-Okoro
    # Conflicts:
    #	app/src/main/java/com/example/uju/coursetracker/objects/Reminder.java
    #	app/src/main/java/com/example/uju/coursetracker/presentation/DueDatesActivity.java
    #	app/src/main/res/layout/activity_due_dates.xml
    #	app/src/main/res/values/strings.xml
    
    fixed error , courseTracker crashed onclick of the EITC Maps Tab

--- app/src/main/java/com/example/uju/coursetracker/presentation/MainActivity.java
+++ app/src/main/java/com/example/uju/coursetracker/presentation/MainActivity.java
@@ -184,7 +184,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
 
             case R.id.mapPage:
-                Intent mPage = new Intent(MainActivity.this, PickMapActivity.class);
+                Intent mPage = new Intent(MainActivity.this, E1Activity.class);
                 startActivity(mPage);
                 break;
         }
