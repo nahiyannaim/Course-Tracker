0708d02c435d6e9cb9b7caeab95df1a0d7e315d7
hintaycl <hintaycl@myumanitoba.ca>
2018-03-21 22:04:18 -0500

    Saving changes made to testInvalidCurrentCourses method

--- app/src/test/java/com/example/uju/coursetracker/tests/integration/BusinessPersistenceSeamTest.java
+++ app/src/test/java/com/example/uju/coursetracker/tests/integration/BusinessPersistenceSeamTest.java
@@ -5,11 +5,11 @@ import junit.framework.TestCase;
 
 public class BusinessPersistenceSeamTest extends TestCase
 {
-//    public BusinessPersistenceSeamTest(String arg0)
-//    {
-//        super(arg0);
-//    }
-//
+    public BusinessPersistenceSeamTest(String arg0)
+    {
+        super(arg0);
+    }
+
 //    public void testAccessStudents()
 //    {
 //        AccessStudents as;
