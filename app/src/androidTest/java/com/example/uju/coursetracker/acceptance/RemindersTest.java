de2661d19e6b45dd5c65e7ec3f1febbb9e7a69ee
Uju <hillary.nnamani@hotmail.com>
2018-03-23 00:03:27 -0500

    Did All the Acceptance tests for the Reminders objects

--- app/src/androidTest/java/com/example/uju/coursetracker/acceptance/RemindersTest.java
+++ app/src/androidTest/java/com/example/uju/coursetracker/acceptance/RemindersTest.java
@@ -103,9 +103,142 @@ public class RemindersTest extends ActivityInstrumentationTestCase2<MainActivity
         solo.waitForActivity("MainActivity");
 
     }
-        //
 
-        //
+    public void testInvalidAddReminder() {
+        solo.waitForActivity("MainActivity");
+        solo.clickOnImageButton(0);
+        solo.clickOnMenuItem("My Reminders");
+        solo.assertCurrentActivity("Expected activity DueDatesActivity", "DueDatesActivity");
+
+        //TEST W/OUT CLICKING ON COURSE
+        solo.clickOnButton("Add Reminder");
+        solo.assertCurrentActivity("Expected activity CreateNewReminder Activity", "CreateNewReminderActivity");
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+
+        //TEST IF YOU PUT DATE W/OUT CLICKING ON COURSE
+        solo.clearEditText(0);
+        solo.enterText(0, "07/23/2018");
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+        //TEST IF YOU ADD REM TYPE W/OUT CLICKING ON COURSE
+        boolean actual = solo.searchText("Reminder Type:");
+        assertEquals("RemType text not found", true, actual);
+
+        solo.pressSpinnerItem(0, 1);
+        actual = solo.isSpinnerTextSelected(0, "Quiz");
+        assertEquals("Spinner is not Quiz", true, actual);
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+        //TEST ALL PATHS W/OUT CLICKING ON COURSE
+        actual = solo.searchText("Reminder Type:");
+        assertEquals("RemType text not found", true, actual);
+
+        solo.pressSpinnerItem(0, 1);
+        actual = solo.isSpinnerTextSelected(0, "Midterm");
+        assertEquals("Spinner is not Midterm", true, actual);
+
+        solo.clearEditText(0);
+        solo.enterText(0, "07/30/2019");
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+    }
+    public void testInvalidDate()
+    {
+        solo.waitForActivity("MainActivity");
+        solo.clickOnImageButton(0);
+        solo.clickOnMenuItem("My Reminders");
+        solo.assertCurrentActivity("Expected activity DueDatesActivity", "DueDatesActivity");
+
+        //ADD REMINDER
+        solo.clickOnButton("Add Reminder");
+        solo.assertCurrentActivity("Expected activity CreateNewReminder Activity", "CreateNewReminderActivity");
+
+        Assert.assertTrue(solo.searchText("ECON 1020"));
+        Assert.assertTrue(solo.searchText("A+"));
+        solo.clickOnText("ECON 1020");
+
+
+        boolean actual = solo.searchText("Reminder Type:");
+        assertEquals("RemType text not found", true, actual);
+
+        solo.pressSpinnerItem(0, 0);
+        actual = solo.isSpinnerTextSelected(0, "Assignment");
+        assertEquals("Spinner is not Assignment", true, actual);
+
+        //TEST FOR DIGIT <10
+        solo.clearEditText(0);
+        solo.enterText(0,"07/10/18");
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+        //TEST FOR DIGIT >10
+        solo.clearEditText(0);
+        solo.enterText(0,"07/10/201818");
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+        //TEST FOR INVALID MONTH
+        solo.clearEditText(0);
+        solo.enterText(0,"13/10/2018");
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+        //TEST FOR INVALID DAY
+        solo.clearEditText(0);
+        solo.enterText(0,"07/32/2018");
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+        //TEST FOR DATE WITHOUT "/" BU "-"
+        solo.clearEditText(0);
+        solo.enterText(0,"07-10-2018");
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+        //TEST FOR DATE BEFORE THE PRESENT DATE
+        solo.clearEditText(0);
+        solo.enterText(0,"03/20/2017");
+
+
+        solo.clickOnButton("DONE");
+        solo.waitForDialogToOpen();
+        Assert.assertTrue(solo.searchText("Warning"));
+        solo.goBack();
+
+    }
+
 
 //        Assert.assertTrue(solo.searchText("400: Mary Bailey"));
 //        solo.clickOnText("400: Mary Bailey");
